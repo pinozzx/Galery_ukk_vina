630db0a6d3a23a5b3d012e9448b580194b2eb257
pinozzx <pinozzzx@gmail.com>
2024-03-01 23:50:59 +0700

    Galery_ukk_vina

---
+++ resources/views/register.blade.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title> Galery Ukk | Register</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="{{ asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
+</head>
+<body class="hold-transition register-page">
+<div class="register-box">
+  <div class="register-logo">
+    <a href="#"><b>Galery</b>Ukk</a>
+  </div>
+
+  <div class="card">
+    <div class="card-body register-card-body">
+      <p class="login-box-msg">Register</p>
+
+      <form action={{ route('postregister') }} method="post">
+        @csrf
+        <div class="input-group mb-3">
+          <input type="text" class="form-control @error('name') is-invalid @enderror" placeholder="Nama" name="name" value="{{ old('name') }}">
+         
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user"></span>
+            </div>
+          </div>
+         
+        </div>
+        
+           
+         
+        <div class="input-group mb-3">
+          <input type="text" class="form-control @error('username') is-invalid @enderror" placeholder="Username"  name="username" value="{{ old('username') }}">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user"></span>
+            </div>
+          </div>
+         
+        </div>
+        
+        <div class="input-group mb-3">
+          <input type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email"  name="email" value="{{ old('email') }}">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+          
+        </div>
+        
+        <div class="input-group mb-3">
+          <input type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password"  name="password" value="{{ old('password') }}">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+          
+        </div>
+        
+        <div class="input-group mb-3">
+          <input type="password" class="form-control @error('repassword') is-invalid @enderror" placeholder="Retype password"  name="repassword" value="{{ old('repassword') }}">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+          
+        </div>
+        
+        <div class="row">
+          <div class="col-8">
+            <div class="icheck-primary">
+              <input type="checkbox" id="agreeTerms" name="terms" value="agree">
+              <label for="agreeTerms">
+               Saya setuju dengan<a href="#">persyaratan</a>
+              </label>
+            </div>
+          </div>
+          
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block">Register</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+
+    
+
+      <a href="/login" class="text-center">Saya sudah punya akun</a>
+    </div>
+    <!-- /.form-box -->
+  </div><!-- /.card -->
+</div>
+<!-- /.register-box -->
+
+<!-- jQuery -->
+<script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset('dist/js/adminlte.min.js') }}"></script>
+</body>
+</html>
